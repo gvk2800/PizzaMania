753f7344ec96ed13da1a8a14725630d651b0ae51
gvk2800 <gauraavkumarcs2800@gmail.com>
2022-04-23 23:05:49 +0530

    add cart page and autherization

--- server.js
+++ server.js
@@ -7,14 +7,26 @@ const PORT=process.env.PORT || 33000
 
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+
 
 //Set template
 app.use(expresslayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
+
 
 app.listen(PORT , ()=>{
     console.log(`Listening at port ${PORT}`)
