29f39f64029213f76f8b98dccaaada3065a9453e
gvk2800 <gauraavkumarcs2800@gmail.com>
2022-04-27 10:30:04 +0530

    re

--- server.js
+++ server.js
@@ -6,16 +6,15 @@ const expresslayout=require('express-ejs-layouts')
 const PORT=process.env.PORT || 33000
 const mongoose = require('mongoose')
 
-
 //Database connection
-const url ='mongodb://localhost/pizza';
-mongoose.connect(url, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Database connected...');
-}).catch(err => {
-    console.log('Connection failed...')
-});
+// const url ='mongodb://localhost/PizzaMania';
+// mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log('Database connected...');
+// }).catch(err => {
+//     console.log('Connection failed...')
+// });
 
 //Assets
 app.use(express.static('public'))
